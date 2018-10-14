2bd207bc8535049c2a9e0947583a6516089e3f30
reza-eghbali <engr.eghbali@gmail.com>
2018-10-14 23:23:26 +0330

    its good to go!

--- vacuum.py
+++ vacuum.py
@@ -11,76 +11,115 @@ percept=[2,1,1]
 distanceSens=1
 flag=0
 moves=0
-
-while flag !=1:
-    def discusion(percept):
+action=""
+def discusion(percept):
 
         action=random.choice(["left","right","up","down"])
         log[percept[0]][percept[1]]=percept[2]
-        print(log)
+        
+        print("Env Map:")
+        for row in log:
+            print(row)
 
+
+        print("\n")
         if percept[2]==1:
             action="suck"
         else:
             i=1
-            if log[percept[0]+i][percept[1]]==0 or log[percept[0]+i][percept[1]]==-1 and percept[0]<len(EnvMap):
-                action="down"
-                moves=i
-            if log[percept[0]-i][percept[1]]==0 or log[percept[0]-i][percept[1]]==-1 and percept[0]>0:   
-                action="up"
-                moves=i
-            if log[percept[0]][percept[1]+i]==0 or log[percept[0]][percept[1]+i]==-1 and percept[1]<len(EnvMap[0]):
-                action="right"
-                moves=i
-            if log[percept[0]][percept[1]-i]==0 or log[percept[0]][percept[1]-i]==-1 and percept[1]>0:
-                action="left"    
-                moves=i
+            if  percept[0]<len(EnvMap)-1:
+                if log[percept[0]+i][percept[1]]==0:  
+                   action="down"
+                   moves=i
+                if log[percept[0]+i][percept[1]]==-1:
+                   action="down"    
+                   moves=i
+                   return action   
+                if log[percept[0]+i][percept[1]]==1:
+                   action="down"
+                   moves=i
+                   return action   
 
-            if log[percept[0]+i][percept[1]]==1:
-                action="down"
-                moves=i
-              
-            if log[percept[0]-i][percept[1]]==1:   
-                action="up"
-                moves=i
-              
-            if log[percept[0]][percept[1]+i]==1:
-                action="right"
-                moves=i
-              
-            if log[percept[0]][percept[1]-i]==1:
-                action="left"    
-                moves=i
-              
+            if  percept[0]>0:
+                if log[percept[0]-i][percept[1]]==0:
+                   action="up"
+                   moves=i 
+                if log[percept[0]-i][percept[1]]==-1:   
+                   action="up"
+                   moves=i
+                   return action   
+                if log[percept[0]-i][percept[1]]==1:   
+                   action="up"
+                   moves=i
+                   return action   
+
+            if percept[1]<len(EnvMap[0])-1:
+                if log[percept[0]][percept[1]+i]==0:
+                   action="right"
+                   moves=i   
+                if log[percept[0]][percept[1]+i]==-1 :
+                   action="right"
+                   moves=i
+                   return action   
+                if log[percept[0]][percept[1]+i]==1:
+                   action="right"
+                   moves=i
+                   return action   
+
+            if percept[1]>0:
+               if log[percept[0]][percept[1]-i]==0:
+                  action="left"    
+                  moves=i
+               if log[percept[0]][percept[1]-i]==-1:
+                  action="left"    
+                  moves=i
+                  return action   
+               if log[percept[0]][percept[1]-i]==1:
+                  action="left"    
+                  moves=i
+                  return action   
+ 
+        
         return action   
 
-    def perception(action):
+def perception(action):
         print(action)
         if action=="down":
-            percept[0]=percept[1]+1
+            percept[0]=percept[0]+1
         if action=="up":
-            percept[0]=percept[1]-1
+            percept[0]=percept[0]-1
         if action=="left":
-            percept[1]=percept[2]-1
+            percept[1]=percept[1]-1
         if action=="right":
-            percept[1]=percept[2]+1
-            
+            percept[1]=percept[1]+1    
         if action=="suck":
            percept[2]=0
-           
+           EnvMap[percept[0]][percept[1]]=0
+        
         log[percept[0]][percept[1]]=EnvMap[percept[0]][percept[1]]
+
         percept[2]=EnvMap[percept[0]][percept[1]]
+
         return percept
 
-    def checker():
+def checker():
 
         for row in log:
             for elem in row:
-                if elem==1:
+                
+                if elem==-1 or elem==1:
                     return False
         else:
             return True            
 
-    act=discusion(percept)
-    checker()        
-    perception(act)
+
+
+
+while flag !=1:
+    
+    action=discusion(percept)
+    if checker():
+        print("all clear!")
+        exit()
+    else:    
+        percept=perception(action)
